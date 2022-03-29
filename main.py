94561c75d1a3af7bd78bf8d1d578caa434d62d55
Muhammad Daffa Ferdiansyah <daffaferdiansyah48@gmail.com>
2022-03-29 11:31:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,22 +6,26 @@ def generateChromosome():
     chromosome = []
     for _ in range(6):
         chromosome.append(random.randint(0, 9))
+
     return chromosome
 
 def decodeX(chromosome):
     #mendekode nilai x
     bot, top = (-5, 5) #-5 <= x <= 5
+
     return bot + ((top - bot)/(9 * 10**(-1) + 9 * 10**(-2)+ 9 * 10**(-3))) * (chromosome[0] * 10 ** (-1) + chromosome[1] * 10 ** (-2) + chromosome[2] * 10 ** (-3))
 
 def decodeY(chromosome):
     #mendekode nilai y
     bot, top = (-5, 5) #-5 <= y <= 5
+
     return bot + ((top - bot)/(9 * 10**(-1) + 9 * 10**(-2)+ 9 * 10**(-3))) * (chromosome[0] * 10 ** (-1) + chromosome[1] * 10 ** (-2) + chromosome[2] * 10 ** (-3))
 
-def fitness(chromosome):
+def fitnessFunction(chromosome):
     #menghitung nilai fitness
     x = decodeX(chromosome)
     y = decodeY(chromosome)
+
     return ((math.cos(x)+math.sin(y))**2) / (x**2 + y**2)
 
 def generatePopulation(pop_size):
@@ -30,21 +34,45 @@ def generatePopulation(pop_size):
     for i in range(pop_size):
         chromosome = generateChromosome()
         population.append(chromosome)
+
     return population
 
 def calculateFitness(population):
+    fitness = []
     for i in range(len(population)):
-        fitness_temp = fitness(population[i])
-        population[i].append(fitness_temp)
-    return population
+        fitness_temp = fitnessFunction(population[i])
+        fitness.append(fitness_temp)
+        print(fitness[i])
+    return fitness
 
 def fitnessSort(population):
-    step = 1
-    while step < len(population):
+    fitness = calculateFitness(population)
+    pas = 1
+    while pas < len(population):
+        idx = pas - 1
+        i = pas
+        while i < len(population):
+            if fitness[idx] > fitness[i]:
+                idx = i
+            i += 1
+        temp = population[pas-1]
+        temp_fit = fitness[pas-1]
+        population[pas-1] = population[idx]
+        fitness[pas-1] = fitness[idx]
+        population[idx] = temp
+        fitness[idx] = temp_fit
+        pas += 1
+
+    return population
+
+def tournamentSelection(population):
+    
+
+   
         
 
 pop = generatePopulation(10)
-calculateFitness(pop)
+
 print(pop)
 fitnessSort(pop)
 print(pop)
